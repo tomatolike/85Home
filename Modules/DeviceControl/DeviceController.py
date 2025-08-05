397d8fd128d51b6cbaa44ec006d1048b84f58afa
tomatolike <695268122@qq.com>
2025-08-04 22:20:53 -0700

    try fix 7

--- Modules/DeviceControl/DeviceController.py
+++ Modules/DeviceControl/DeviceController.py
@@ -180,7 +180,7 @@ class DeviceController:
         self.m_devices.update(await SwitchBotDevice.discorverDevices())
 
     def sync_update_devices(self):
-        asyncio.run(self.updateDevices)
+        asyncio.run(self.updateDevices())
 
     def getDevicesInfo(self):
         result = []
@@ -198,11 +198,11 @@ class DeviceController:
             retry_time = 5
             while retry_time > 0:
                 try:
-                    asyncio.run(self.m_devices[alias].update_status)
+                    asyncio.run(self.m_devices[alias].update_status())
                     if self.m_devices[alias].get_status() == statuses[index]:
                         break
-                    asyncio.run(self.m_devices[alias].change_status, statuses[index])
-                    asyncio.run(self.m_devices[alias].update_status)
+                    asyncio.run(self.m_devices[alias].change_status(statuses[index]))
+                    asyncio.run(self.m_devices[alias].update_status())
                     if self.m_devices[alias].get_status() == statuses[index]:
                         break
                     retry_time -= 1
